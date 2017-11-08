79608b71d0a06e58ddad22f9604649da08a81f96
FPMCCANN\kevin.robinson <KRobinson@fpmccann.co.uk>
2017-11-08 16:22:03 +0000

    First

---
+++ ExmployeeReviews/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ExmployeeReviews.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ExmployeeReviews/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExmployeeReviews.WebApiApplication" Language="C#" %>
